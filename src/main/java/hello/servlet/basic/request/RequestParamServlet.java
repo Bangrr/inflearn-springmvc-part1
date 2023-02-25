fae40641c0b8ceec8c54e7d217092dcb17a12198
Mark <qudfbftnqls@naver.com>
2023-02-25 10:26:32 +0900

    Http 요청 데이터 - GET 쿼리 파라미터
    - RequestParamServlet 생성
    - 메서드를 통한 전체, 단일 파라미터 조회
    - 이름이 같은 복수 파라미터 조회

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,72 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ * <p></p>
+ * 2. 동일한 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&username=kim&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        /*
+        Enumeration<String> parameterNames = request.getParameterNames(); // 파라미터 이름들 모두 조회
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            System.out.println(paramName + "=" + request.getParameter(paramName));
+        }
+        */
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username"); // 단일 파라미터 조회
+        String age = request.getParameter("age");
+
+        System.out.println("request.getParameter(username) = " + username);
+        System.out.println("request.getParameter(age) = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
+        String[] usernames = request.getParameterValues("username"); // 복수 파라미터 조회
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+        }
+
+//        Map<String, String[]> parameterMap = request.getParameterMap(); // 파라미터를 Map으로 조회
+
+        response.getWriter().write("ok");
+    }
+}
+
+/*
+ * [ GET 쿼리 파라미터 방식 ]
+ * 메시지 바디 없이 URL의 쿼리 파라미터를 사용해서 데이터를 전달
+ * ex) 검색, 필터, 페이징 등에서 많이 사용하는 방식
+ * 쿼리 파라미터는 URL에 '?'를 시작으로 보낼 수 있고 추가 파라미터는 '&'로 구분하면 된다.
+ *
+ * username=hello&username=kim 과 같이 파라미터 이름은 하나인데 값이 중복일 경우,
+ * request.getParameterValues() 를 사용해야 한다.
+ * 중복일 때 request.getParameter()를 사용하면 request.getParameterValues() 의 첫 번째 값을 반환한다.
+ *
+ */
