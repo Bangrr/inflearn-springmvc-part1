9aa9ace8d4167502faf392ca29e7dabc997f9517
Mark <qudfbftnqls@naver.com>
2023-03-12 08:26:18 +0900

    서블릿으로 회원 관리 애플리케이션 만들기
    - 회원 등록 폼 ( MemberFormServlet.java )
    - 회원 저장 ( MemberSaveServlet.java )
    - 회원 목록 ( MemberListServlet.java )

---
+++ src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -0,0 +1,68 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll(); // 모든 회원을 조회한다.
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        w.write("<html>");
+        w.write("<head>");
+        w.write(" <meta charset=\"UTF-8\">");
+        w.write(" <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write(" <thead>");
+        w.write(" <th>id</th>");
+        w.write(" <th>username</th>");
+        w.write(" <th>age</th>");
+        w.write(" </thead>");
+        w.write(" <tbody>");
+        /*
+        w.write(" <tr>");
+        w.write(" <td>1</td>");
+        w.write(" <td>userA</td>");
+        w.write(" <td>10</td>");
+        w.write(" </tr>");
+        */
+        for (Member member : members) { // 회원 수 만큼 동적으로 html 에 데이터를 나타낸다.
+            w.write(" <tr>");
+            w.write(" <td>" + member.getId() + "</td>");
+            w.write(" <td>" + member.getUsername() + "</td>");
+            w.write(" <td>" + member.getAge() + "</td>");
+            w.write(" </tr>");
+        }
+        w.write(" </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
+    }
+}
+
+/*
+ * 서블릿과 자바 코드만으로 HTML 을 만들었다.
+ * 템플릿 엔진을 사용하면 HTML 문서에서 필요한 곳만 코드를 적용해서 동적으로 변경할 수 있다.
+ * 템플릿 엔진에는 JSP, Thymeleaf, Freemarker, Velocity 등이 있다.
+ */
