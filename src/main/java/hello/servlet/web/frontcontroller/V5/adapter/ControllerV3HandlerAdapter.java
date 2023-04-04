24b4c6e5873fd083e439e66705468dfb21ce2f2c
Mark <qudfbftnqls@naver.com>
2023-04-05 00:14:13 +0900

    MVC 프레임워크 만들기 V5 - 어댑터 패턴
    - 어댑터용 인터페이스 (MyHandlerAdapter.java)
    - ControllerV3 처리 어댑터 구현 (ControllerV3HandlerAdapter.java)

---
+++ src/main/java/hello/servlet/web/frontcontroller/V5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,38 @@
+package hello.servlet.web.frontcontroller.V5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.V3.ControllerV3;
+import hello.servlet.web.frontcontroller.V5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3); // handler 가 V3컨트롤러이면 True, 아니면 False 반환
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler; // supports 메서드에서 한번 검증했기 때문에 casting 해도 된다.
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> {
+            paramMap.put(paramName, request.getParameter(paramName));
+        });
+        return paramMap;
+    }
+}
